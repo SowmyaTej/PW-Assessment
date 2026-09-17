c49fbf224b0c375cd6f6758d5651499420562bb2
Sowmya <reachmebysowmya@gmail.com>
2026-09-17 16:10:30 +0530

    Add login scenarios and test data

---
+++ test-data/loginData.ts
@@ -0,0 +1,10 @@
+export const loginData = { 
+    validUser: { 
+        email: 'pw@abc.com', 
+        password: 'Test@1' 
+    },
+
+    invalidUser: {
+         email: 'invalid@abc.com', 
+         password: 'Invalid@123' }
+ };
--- tests/loginpage.spec.ts
+++ tests/loginpage.spec.ts
@@ -1,5 +1,6 @@
 import { test, expect } from '@playwright/test';
 import { LoginPage } from '../pages/LoginPage';
+import { loginData } from '../test-data/loginData';
 //import { HomePage } from '../pages/HomePage';
 
 test('verify valid login', async ({ page }) => {
@@ -7,9 +8,19 @@ test('verify valid login', async ({ page }) => {
     //AAA
     let loginPage = new LoginPage(page);
     await loginPage.goToLoginPage();
-    const actualTitle = await loginPage.doLogin('pw@abc.com', 'Test@1');
+    const actualTitle = await loginPage.doLogin( loginData.validUser.email, loginData.validUser.password );
     await expect(page).toHaveTitle(actualTitle);
 
-})
+});
+
+test('verify Invalid login', async ({ page }) => {
+    //AAA
+    let loginPage = new LoginPage(page);
+    await loginPage.goToLoginPage();
+    await loginPage.doLogin( loginData.invalidUser.email, loginData.invalidUser.password );
+    const errorMesg = await loginPage.getInvalidLoginMessage();
+    expect(errorMesg).toContain('Warning: No match for E-Mail Address and/or Password.')
+
+});
 
     
